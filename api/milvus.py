10f1a046ed58a45e32b94fe37f8488cb638dd33a
Quentin Bonnard <quentin.github@monsieurcube.fr>
2023-05-01 15:05:13 +0200

    Automatically change default milvus password

--- api/milvus.py
+++ api/milvus.py
@@ -7,11 +7,14 @@ from pymilvus import (
     DataType,
     Collection,
     utility,
+    MilvusException,
 )
 
 import os
 
 from common import feature_dimensions, MAX_METADATA_LENGTH
+
+DEFAULT_ROOT_PASSWORD = "Milvus"
 
 
 def ensure_connection():
@@ -21,8 +24,25 @@ def ensure_connection():
     host, _, port = os.environ.get("MILVUS_URL", "").partition(":")
     host = host or "127.0.0.1"
     port = port or "19530"
+    password = os.environ.get("MILVUS_PASSWORD", DEFAULT_ROOT_PASSWORD)
+    if password == DEFAULT_ROOT_PASSWORD:
+        print("!!! Set the MILVUS_PASSWORD environment variable !!!")
+        return
 
-    connections.connect(host=host, port=port)
+    # Change the root password to the specified if the default one is in use
+    try:
+        connections.connect(host=host, port=port, user="root", password=password)
+        utility.list_collections()  # Actually triggers the auth check failure
+    except MilvusException as e:
+        if "auth check failure" in str(e):
+            connections.connect(
+                host=host, port=port, user="root", password=DEFAULT_ROOT_PASSWORD
+            )
+            utility.reset_password("root", DEFAULT_ROOT_PASSWORD, password)
+            print("Root password set")
+        else:
+            raise e
+            # raise MilvusException("Failed to connect to Milvus server") from e
 
 
 # https://github.com/towhee-io/examples/blob/9d199df094e3ec96a0764485ef48285b70be4193/image/reverse_image_search/1_build_image_search_engine.ipynb
